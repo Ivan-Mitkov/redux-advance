c225336346b44572ece7050f8ad7e9ed62581fca
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-20 15:40:40 +0300

    add configuration for react devtools

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers,applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
-import counterReducer from './store/reducers/counter';
-import resultReducer from './store/reducers/result';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import counterReducer from "./store/reducers/counter";
+import resultReducer from "./store/reducers/result";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 const rootReducer = combineReducers({
-    ctr: counterReducer,
-    res: resultReducer
+  ctr: counterReducer,
+  res: resultReducer
 });
 //create simple middleware for logging
-const logger=store=>{
-    return next=>{
-        return action =>{
-            console.log('Middleware Dispaching: ',action);
-            const result = next(action);
-            console.log('Middleware next: ', store.getState())
-            return result;
-        }
-    }
-}
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("Middleware Dispaching: ", action);
+      const result = next(action);
+      console.log("Middleware next: ", store.getState());
+      return result;
+    };
+  };
+};
+//devtool usage ////////https://github.com/zalmoxisus/redux-devtools-extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+////////
 //we can pass list of middleware function, they will be executed in order
-const store = createStore(rootReducer,applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
