378da49983ed8253f6f90db238a1f8271963561d
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-20 15:07:24 +0300

    add simple logger middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers,applyMiddleware } from 'redux';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -13,8 +13,19 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
-
-const store = createStore(rootReducer);
+//create simple middleware for logging
+const logger=store=>{
+    return next=>{
+        return action =>{
+            console.log('Middleware Dispaching: ',action);
+            const result = next(action);
+            console.log('Middleware next: ', store.getState())
+            return result;
+        }
+    }
+}
+//we can pass list of middleware function, they will be executed in order
+const store = createStore(rootReducer,applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
