a16599361390776b0973dc6f55c9d24a64c8404b
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-20 17:22:47 +0300

    leaner switch statements

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,7 +1,17 @@
 import * as actionTypes from "../actions/actionTypes";
 import { updateObject } from "./../utility";
+
 const initialState = {
   results: []
+};
+const deleteResult = (state, action) => {
+  // const id = 2;
+  // const newArray = [...state.results];
+  // newArray.splice(id, 1)
+  const updatedArray = state.results.filter(
+    result => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
 };
 
 const reducer = (state = initialState, action) => {
@@ -11,13 +21,7 @@ const reducer = (state = initialState, action) => {
         results: state.results.concat({ id: new Date(), value: action.result })
       });
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1)
-      const updatedArray = state.results.filter(
-        result => result.id !== action.resultElId
-      );
-      return updateObject(state, { results: updatedArray });
+      return deleteResult(state, action);
     default:
       return state;
   }
